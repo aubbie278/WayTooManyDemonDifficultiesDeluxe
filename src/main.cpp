158869cf8cc31f822739515b0c5c2d30cca5f00d
pearsenjoyer <pearsenjoyer@hotmail.com>
2026-02-27 13:09:32 -0500

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+/**
+ * Include the Geode headers.
+ */
+#include <Geode/Geode.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/utils/web.hpp>
+#include <Geode/loader/Event.hpp>
+
+/**
+ * Brings cocos2d and all Geode namespaces to the current scope.
+ */
+using namespace geode::prelude;
+
+class $modify(LevelInfoLayer) {
+	struct Fields {
+		EventListener<web::WebTask> m_listListener{};
+	};
+
+	void drawDemon(std::string_view resource) {
+		auto demonSprite{ CCSprite::create(resource.data()) };
+		demonSprite->setPosition({ 184.5, 231.0 });
+		this->addChild(demonSprite);
+	}
+
+
+	bool init(GJGameLevel* level, bool challenge) {
+		if (!LevelInfoLayer::init(level, challenge)) {
+			return false;
+		}
+
+		m_fields->m_listListener.bind([this](web::WebTask::Event* event) {
+			if (web::WebResponse * result{ event->getValue() }) {
+				log::debug("response {}", result->string().unwrap());
+				std::string rawJson{ result->string().unwrap() };
+				auto parsedJson{ matjson::parse(rawJson).unwrapOrDefault() };
+
+
+				if (parsedJson.contains("position")) {
+					geode::log::debug("level \"{}\", position {}", parsedJson["name"].asString().unwrap(), parsedJson["position"].asInt().unwrap());
+					intmax_t position{ parsedJson["position"].asInt().unwrap() };
+					this->getChildByID("difficulty-sprite")->setVisible(false);
+						
+					if (position <= 150 && position > 75) {
+						drawDemon("cardemon.png"_spr);
+					}
+					else if (position <= 75) {
+						drawDemon("uwudemon.png"_spr);
+					}
+
+				} else {
+					log::debug("not extreme demon");
+				}
+			}
+	    });
+
+		auto request{ web::WebRequest() };
+		log::debug("{}", level->m_levelID);
+		m_fields->m_listListener.setFilter(request.get(fmt::format("https://api.aredl.net/v2/api/aredl/levels/{}", level->m_levelID)));
+		return true;
+	}
+};
+
+
